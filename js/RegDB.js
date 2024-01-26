6556ba52f6ea30a49e4ae967c40c07a07112d1ff
AnshuMishra1122003 <anshuamishra1122003@gmail.com>
2024-01-26 13:58:53 +0530

    updated

--- js/RegDB.js
+++ js/RegDB.js
@@ -1,4 +1,4 @@
-﻿import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.3/firebase-app.js";
+﻿import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.2/firebase-app.js";
 
 import {
   getDatabase,
